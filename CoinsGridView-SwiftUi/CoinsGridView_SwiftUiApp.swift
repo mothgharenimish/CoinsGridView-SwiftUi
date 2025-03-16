e520ceaa7bd93294d1fd5580288594baf73005dd
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-16 16:57:59 +0530

    Initial Commit

---
+++ CoinsGridView-SwiftUi/CoinsGridView_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsGridView_SwiftUiApp.swift
+//  CoinsGridView-SwiftUi
+//
+//  Created by Nimish Sharad Mothghare on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinsGridView_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinsGridView-SwiftUiTests/CoinsGridView_SwiftUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsGridView_SwiftUiTests.swift
+//  CoinsGridView-SwiftUiTests
+//
+//  Created by Nimish Sharad Mothghare on 16/03/25.
+//
+
+import Testing
+@testable import CoinsGridView_SwiftUi
+
+struct CoinsGridView_SwiftUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
