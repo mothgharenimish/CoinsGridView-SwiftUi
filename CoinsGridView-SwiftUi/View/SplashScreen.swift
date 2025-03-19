d278d1a49b641751d302934da5d903f50484a785
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-19 15:16:57 +0530

    Xcode Update Changes

---
+++ CoinsGridView-SwiftUi/View/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  CoinsGridView-SwiftUi
+//
+//  Created by Nimish Sharad Mothghare on 18/03/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
